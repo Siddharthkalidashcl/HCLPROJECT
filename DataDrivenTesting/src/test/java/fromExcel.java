e0cfbf3baba7d4236c111e158c86ffb6d46b6984
Siddharthkalidashcl <87986563+Siddharthkalidashcl@users.noreply.github.com>
2021-07-30 18:28:15 +0530

    Update fromExcel.java

--- DataDrivenTesting/src/test/java/fromExcel.java
+++ DataDrivenTesting/src/test/java/fromExcel.java
@@ -1,45 +1,22 @@
-// import java.io.File;
-// import java.io.FileInputStream;
-// import java.io.FileNotFoundException;
-// import java.io.FileOutputStream;
-// import java.io.IOException;
-// import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
-// import org.apache.poi.xssf.usermodel.XSSFSheet;
-// import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.testng.annotations.Test;
+public class DragandDrop {
 
-// public class fromExcel {
-
-// 	@Test
-// 	public void fromexcel() throws IOException {
-// 		System.setProperty("webdriver.chrome.driver", "F:\\QSPIDER SQL\\HCL\\DataDrivenTesting\\Drivers\\chromedriver.exe");
-// 		WebDriver aa = new ChromeDriver();
-// 		String file=("C:\\Users\\sid\\Desktop\\HCL ASSIGNMENT\\ReadFile.xlsx");
-// 		File ff=new File(file);
-// 		FileInputStream fe=new FileInputStream(ff);
-// 		XSSFWorkbook ww1=new XSSFWorkbook(fe);  
-// 		XSSFSheet s1= ww1.getSheetAt(0);
-// 		int row=s1.getLastRowNum();
-// 		for(int i=1;i<row+1;i++)
-// 		{
-// 			String username=s1.getRow(i).getCell(0).getStringCellValue();
-// 			String pass=s1.getRow(i).getCell(1).getStringCellValue();
-// 			aa.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-// 		aa.get("https://www.saucedemo.com/");
-
-// 		aa.findElement(By.xpath("//input[@id='user-name']")).click();
-// 		aa.findElement(By.xpath("//input[@id='user-name']")).sendKeys(username);
-// 		aa.findElement(By.xpath("//input[@id='password']")).click();
-// 		aa.findElement(By.xpath("//input[@id='password']")).sendKeys(pass);
-// 		aa.findElement(By.xpath("//input[@id='user-name']")).clear();
-// 		aa.findElement(By.xpath("//input[@id='password']")).clear();
-		
-// 	}
-		
-// 	}
-
-// }
+	@Test
+	public void DragandDropModule() {
+		System.setProperty("webdriver.chrome.driver", "F:\\QSPIDER SQL\\HCL\\WEBDRIVER1\\Drivers\\chromedriver.exe");
+		WebDriver aa = new ChromeDriver();
+		aa.get("http://leafground.com/pages/drag.html");
+		WebElement e1=aa.findElement(By.xpath("//div[@id='draggable']"));
+		Actions ac=new Actions(aa);
+		ac.dragAndDropBy(e1, 215, 57);
+		ac.build().perform();
+	}
+}
